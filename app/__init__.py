347556098cfd9777345efd1e2a851dd9b6fadf0f
evans <evansnwadi@gmail.com>
2023-11-21 01:11:06 +0100

    form vaidation and blog

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from datetime import timedelta
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'EVANSXCHANGE'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config["SQALCHEMY_TRACK_MODIFICATIONS"] = False
+app.permanent_session_lifetime = timedelta(minutes=5)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from app import route
