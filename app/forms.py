ff58193136f633e5f95bf0f67c5c17d931b11c6c
evans <evansnwadi@gmail.com>
2023-11-25 01:10:39 +0100

    verification sms

--- app/forms.py
+++ app/forms.py
@@ -54,10 +54,25 @@ class UpdateAccountForm(FlaskForm):
           if password.data != current_user.password:
                user = User.query.filter_by(password=password.data).first()
                if user:
-                    raise ValidationError('This password already exist... Please use another email')               
+                    raise ValidationError('This password already exist... Please use another password')               
      
       
 class PostForm(FlaskForm):
      title = StringField('Title', validators=[DataRequired()])
      content = TextAreaField('Content', validators=[DataRequired()])
      submit = SubmitField('Post')
+
+class RequestResetForm(FlaskForm):
+     email = StringField('Email',  validators=[DataRequired(), Email()])   
+     submit = SubmitField('Request Reset Password')
+
+     def validate_email(self, email):
+          user = User.query.filter_by(email=email.data).first()
+          if user is None:
+               raise ValidationError('There is no account with that email. you must register first.' 'danger')
+     
+
+class ResetPasswordForm(FlaskForm):
+     password = PasswordField('Password', validators=[DataRequired()])
+     confirm_password = PasswordField('confirm password', validators=[DataRequired(), EqualTo('password')])
+     submit = SubmitField('Reset Password')
